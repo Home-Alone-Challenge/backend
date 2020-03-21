3c97411f869b5decb52b53d22bb80947224fde20
Philipp Hamm <philipp.hamm@gmail.com>
2020-03-21 15:17:31 +0100

    basic setup

--- app/controllers/api/v1/daily_tips_controller.rb
+++ app/controllers/api/v1/daily_tips_controller.rb
@@ -1,2 +1,5 @@
 class Api::V1::DailyTipsController < ApplicationController
+  def create
+
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -35,5 +35,4 @@ ActiveRecord::Schema.define(version: 2020_03_21_121740) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
