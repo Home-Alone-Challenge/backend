100e3443d82f814882d575500d10458a51afa023
Philipp Hamm <philipp.hamm@gmail.com>
2020-03-21 21:06:09 +0100

    Cors management part 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      # Allow CORS Request
-      match '*all', controller: 'application', action: 'cors_preflight_check', via: [:options]
-
       root to: 'challenges#index'
       resources :users, only: [:show, :create] do
         resources :challenges, only: [:show, :new, :create, :edit, :index, :destroy]
