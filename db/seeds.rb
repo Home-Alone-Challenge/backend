5499567e6972f7a2f602dcf6ef77e0f455181156
Philipp Hamm <philipp.hamm@gmail.com>
2020-03-21 17:50:22 +0100

    please check this code

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+csv_options = { col_sep: ',', headers: :first_row }
+filepath    = 'dailytips.csv'
+puts filepath
+puts "setup"
+CSV.foreach(filepath, csv_options) do |row|
+  dailytip = Dailytip.new
+  dailytip.title = row['Title']
+  dailytip.description = row['Description']
+  dailytip.save
+end
