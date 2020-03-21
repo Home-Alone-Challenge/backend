0ab15894e08ba87b0f652de613550fe9e4d4b300
Philipp Hamm <philipp.hamm@gmail.com>
2020-03-21 15:36:35 +0100

    superbasic parse csv function

--- app/controllers/api/v1/daily_tips_controller.rb
+++ app/controllers/api/v1/daily_tips_controller.rb
@@ -1,5 +1,14 @@
+require 'csv'
+
 class Api::V1::DailyTipsController < ApplicationController
   def create
-
+    csv_options = { col_sep: ',', headers: :first_row }
+    filepath    = 'dailytips.csv'
+    CSV.foreach(filepath, csv_options) do |row|
+      dailytip = Dailytip.new
+      dailytip.title = row['Title']
+      dailytip.description = row['Description']
+      dailytip.save
+    end
   end
 end
