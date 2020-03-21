a8db42d687719daa3270f87239f997d84a02e5e4
Mariken Gulpen <marikenvangulpen@gmail.com>
2020-03-21 16:52:05 +0100

    added a random method to show in controller

--- app/controllers/api/v1/challenges_controller.rb
+++ app/controllers/api/v1/challenges_controller.rb
@@ -1,17 +1,23 @@
 class Api::V1::ChallengesController < ApplicationController
   before_action :set_id, only: [:show, :edit, :update, :destroy]
+
   def index
     @challenges = Challenge.all
     render json: @challenges
   end
+
   def show
+    @challenge_random = Challenge.all.sample
     render json: @challenge
   end
+
   def new
-    @challenge = Challenge.new(challenge_params)
+    @challenge = Challenge.new
   end
+
   def edit
   end
+
   def create
     @challenge = Challenge.new(challenge_params)
       if @challenge.save
@@ -20,22 +26,24 @@ class Api::V1::ChallengesController < ApplicationController
         render error: { error: 'Aufgabe wurde nicht erstellt. Bitte überprüfe deine Eingaben.'}
       end
   end
+
   def update
     @challenge.update(challenge_params)
     render json: @challenge
   end
+
   def destroy
     @challenge.destroy
   end
-  def random_challenge
-    @challenge = Challenge.all.sample
-    render json: @challenge
-  end
+
   private
+
   def set_id
     @challenge = Challenge.find(params[:id])
   end
+
   def challenge_params
     params.require(:challenge).permit(:title, :description, :duration, :category)
   end
+
 end
