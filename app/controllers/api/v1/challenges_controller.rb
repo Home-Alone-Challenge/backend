f110fa82ca8b59691110eae794299a22f63cde9d
Mariken Gulpen <marikenvangulpen@gmail.com>
2020-03-21 13:53:22 +0100

    controller actions show new create

--- app/controllers/api/v1/challenges_controller.rb
+++ app/controllers/api/v1/challenges_controller.rb
@@ -1,2 +1,26 @@
 class Api::V1::ChallengesController < ApplicationController
+
+  def show
+    @challenge = Challenge.find(params[:id])
+    render json @challenge
+  end
+
+  def new
+    @challenge = Challenge.new(challenge_params)
+  end
+
+  def create
+    @challenge = Challenge.new(challenge_params)
+      if @challenge.save
+        render json: @challenge
+      else
+        render error: { error: 'Aufgabe wurde nicht erstellt. Bitte überprüfe deine Eingaben.'}
+      end
+  end
+
+  private
+
+  def challenge_params
+    params.require(:challenge).permit(:title, :description, :duration, :category)
+  end
 end
