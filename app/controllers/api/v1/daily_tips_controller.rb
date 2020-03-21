41cfccda1b2476a7bd95d8f250f3f895aa5cd34c
Beixi Lu <beixi_lu@yahoo.com>
2020-03-21 14:49:44 +0100

    tip#show

--- app/controllers/api/v1/daily_tips_controller.rb
+++ app/controllers/api/v1/daily_tips_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::DailyTipsController < ApplicationController
+  before_action :set_dailytip, only: [:show]
+  def show
+  end
+
+  def set_dailytip
+    @dailytip = Dailytip.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :challenges, only: [:show, :new, :create]
+      resources :dailytips, only: [:show, :create]
     end
   end
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
