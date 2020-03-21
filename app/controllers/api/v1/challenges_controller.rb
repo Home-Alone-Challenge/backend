5ee2dfb0f9032111cd9e1132727c45739d7b2f09
Mariken Gulpen <marikenvangulpen@gmail.com>
2020-03-21 16:04:15 +0100

    changes in controller and seeds challenges

--- app/controllers/api/v1/challenges_controller.rb
+++ app/controllers/api/v1/challenges_controller.rb
@@ -1,14 +1,17 @@
 class Api::V1::ChallengesController < ApplicationController
-
-  def show
-    @challenge = Challenge.find(params[:id])
-    render json @challenge
+  before_action :set_id, only: [:show, :edit, :update, :destroy]
+  def index
+    @challenges = Challenge.all
+    render json: @challenges
   end
-
+  def show
+    render json: @challenge
+  end
   def new
     @challenge = Challenge.new(challenge_params)
   end
-
+  def edit
+  end
   def create
     @challenge = Challenge.new(challenge_params)
       if @challenge.save
@@ -17,9 +20,21 @@ class Api::V1::ChallengesController < ApplicationController
         render error: { error: 'Aufgabe wurde nicht erstellt. Bitte überprüfe deine Eingaben.'}
       end
   end
-
+  def update
+    @challenge.update(challenge_params)
+    render json: @challenge
+  end
+  def destroy
+    @challenge.destroy
+  end
+  def random_challenge
+    @challenge = Challenge.all.sample
+    render json: @challenge
+  end
   private
-
+  def set_id
+    @challenge = Challenge.find(params[:id])
+  end
   def challenge_params
     params.require(:challenge).permit(:title, :description, :duration, :category)
   end
