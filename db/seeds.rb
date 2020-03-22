03ba564512863be03f87db18a49d65ebdc98533f
Beixi Lu <beixi_lu@yahoo.com>
2020-03-22 13:47:23 +0100

    1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
-# require 'csv'
-
-# csv_options = { col_sep: ',', headers: :first_row }
-# filepath    = 'dailytips.csv'
-# puts filepath
-# puts "setup"
-# CSV.foreach(filepath, csv_options) do |row|
-#   dailytip = Dailytip.new
-#   dailytip.title = row['Title']
-#   dailytip.description = row['Description']
-#   dailytip.save
-# end
+require 'csv'
 
 puts 'Cleaning dailytip database'
 Dailytip.destroy_all
+
+csv_options = { col_sep: ',', headers: :first_row }
+filepath    = 'dailytips.csv'
+puts filepath
+puts "setup"
+CSV.foreach(filepath, csv_options) do |row|
+  dailytip = Dailytip.new
+  dailytip.title = row['Title']
+  dailytip.description = row['Description']
+  dailytip.save
+end
 
 puts 'creating Dailytip'
 dailytip = Dailytip.create!(
