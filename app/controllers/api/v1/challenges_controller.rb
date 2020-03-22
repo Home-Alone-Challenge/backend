93b414af8ed772fcd7fa3b91edd7ef6081fe263c
Mariken Gulpen <marikenvangulpen@gmail.com>
2020-03-22 19:11:04 +0100

    added column to challenges and controller challenges done

--- app/controllers/api/v1/challenges_controller.rb
+++ app/controllers/api/v1/challenges_controller.rb
@@ -1,34 +1,13 @@
 class Api::V1::ChallengesController < ApplicationController
   before_action :set_id, only: [:show, :edit, :update, :destroy]
-
   def index
-  @challenges = Challenge.all.where(user_id: params[:user_id])
-  render json: @challenges
+    @challenges = Challenge.all.where(user_id: params[:user_id])
+    render json: @challenges
   end
 
   def random_challenge
-    @random_challenge = Challenge.all.sample
+    @random_challenge = Challenge.all.where(category: params[:category]).sample
     render json: @random_challenge
-  end
-
-  def daily_challenge
-    if Challenge.find_by(daily: true).nil?
-      set_daily
-    end
-    @daily_challenge = Challenge.find_by(daily: true)
-    render json: @daily_challenge
-  end
-
-  def set_daily
-
-    if !Challenge.find_by(daily: true).nil?
-      undaily_challenge = Challenge.find_by(daily: true)
-      undaily_challenge.daily = false
-      undaily_challenge.save
-    end
-    daily_challenge = Challenge.all.sample
-    daily_challenge.daily = true
-    daily_challenge.save
   end
 
   def show
@@ -45,18 +24,19 @@ class Api::V1::ChallengesController < ApplicationController
   end
 
   def create
-    @challenge.user_id = User.id
-    @challenge = Challenge.new(:title, :description, :duration, :category, @challenge.user_id)
-      if @challenge.save
-        render json: @challenge
-      else
-        render error: { error: 'Aufgabe wurde nicht erstellt. Bitte überprüfe deine Eingaben.'}
-      end
+    @challenge.user_id = params[:user_id]
+    @challenge = Challenge.new(challenge_params)
+    if @challenge.save
+      redirect_to api_v1_user_challenge_path(@challenge)
+    else
+      render error: { error: 'Aufgabe wurde nicht erstellt. Bitte überprüfe deine Eingaben.'}
+    end
   end
 
   def update
+    @challenge.user_id = params[:user_id]
     @challenge.update(challenge_params)
-    render json: @challenge
+    redirect_to api_v1_user_challenge_path(@challenge)
   end
 
   def destroy
